249e62295cd24406a4bb222542c6809807b42d25
louis <louis@naiaden.nl>
2014-06-12 11:17:59 +0200

    mbt 3.2.10

---
+++ Formula/mbt.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Mbt < Formula
+  homepage ""
+  url "http://software.ticc.uvt.nl/mbt-3.2.10.tar.gz"
+  sha1 "a8d3f22a868ce8de364bed5a987cdd36b00939b7"
+
+  depends_on 'ticcutils' => :build
+  depends_on 'timbl' => :build
+  depends_on 'timblserver' => :build
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+end
