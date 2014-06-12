18c810c94419cc4843f470191a1c1ca0644319be
louis <louis@naiaden.nl>
2014-06-12 11:20:30 +0200

    mbtserver 0.8

---
+++ Formula/mbtserver.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class Mbt < Formula
+  homepage ""
+  url "http://software.ticc.uvt.nl/mbtserver-0.8.tar.gz"
+  sha1 "b610cb0f2a1c01136499ea23b2ab0d958c486bd8"
+
+  depends_on 'ticcutils' => :build
+  depends_on 'timbl' => :build
+  depends_on 'timblserver' => :build
+  depends_on 'mbtserver' => :build
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+end
